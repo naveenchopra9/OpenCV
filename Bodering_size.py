2663c8454a354ab17e2f0557fdf044130fd4d905
Naveen Chopra <31899453+naveenchopra9@users.noreply.github.com>
2018-01-24 19:02:13 +0530

    Create Bodering_size.py

---
+++ Bodering_size.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import CCA
+import cv2
+BLUE = [255,0,0]
+img1 = cv2.imread('/Users/naveen/Desktop/project2.jpg',cv2.IMREAD_COLOR  )
+
+replicate = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REPLICATE)
+reflect = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_REFLECT_101)
+wrap = cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_WRAP)
+constant= cv2.copyMakeBorder(img1,10,10,10,10,cv2.BORDER_CONSTANT,value=BLUE)
+
+plt.subplot(231),plt.imshow(img1,'gray'),plt.title('ORIGINAL')
+plt.subplot(232),plt.imshow(replicate,'gray'),plt.title('REPLICATE')
+plt.subplot(233),plt.imshow(reflect,'gray'),plt.title('REFLECT')
+plt.subplot(234),plt.imshow(reflect101,'gray'),plt.title('REFLECT_101')
+plt.subplot(235),plt.imshow(wrap,'gray'),plt.title('WRAP')
+plt.subplot(236),plt.imshow(constant,'gray'),plt.title('CONSTANT')
+plt.xticks()
+plt.yticks()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
